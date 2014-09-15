f97613a24bfda40f7aa068409962fd716680d2fa
Melissa Huang <melissahuang@airbears2-10-142-1-187.airbears2.1918.berkeley.edu>
2014-09-15 12:42:10 -0700

    add iOS workspace

---
+++ watershed-iOS/Watershed/Watershed/WPViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPViewController.h
+//  Watershed
+//
+//  Created by Melissa Huang on 9/15/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPViewController : UIViewController
+
+@end
---
+++ watershed-iOS/Watershed/WatershedTests/WatershedTests.m
@@ -0,0 +1,34 @@
+//
+//  WatershedTests.m
+//  WatershedTests
+//
+//  Created by Melissa Huang on 9/15/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WatershedTests : XCTestCase
+
+@end
+
+@implementation WatershedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
