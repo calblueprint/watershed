7169e1ecbfdad0e20ad82be2b45dee8e78ccbe13
Mark Miyashita <negativetwelve@gmail.com>
2014-09-27 09:38:57 -0700

    Use devise for login authentication

--- watershed-rails/config/routes.rb
+++ watershed-rails/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
-      match "login", to: "sessions#login", via: :post
+      devise_for :users
     end
   end
 
