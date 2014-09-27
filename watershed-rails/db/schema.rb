f8a4c3057c287d9bd6aebed2b55268a61cdd4b98
Mark Miyashita <negativetwelve@gmail.com>
2014-09-27 15:22:20 -0700

    Add name and role to users

--- watershed-rails/db/schema.rb
+++ watershed-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927164410) do
+ActiveRecord::Schema.define(version: 20140927222102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140927164410) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
