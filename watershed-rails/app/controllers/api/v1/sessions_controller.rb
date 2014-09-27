077bdcc14419b21c1ae4cd25fee6200f048dc2f4
Mark Miyashita <negativetwelve@gmail.com>
2014-09-27 09:35:12 -0700

    Add create method for sessions

--- watershed-rails/app/controllers/api/v1/sessions_controller.rb
+++ watershed-rails/app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,20 @@
 class API::V1::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  def create
+    user = User.find_for_database_authentication(email: params[:user][:email])
+    return invalid_login_attempt if user.nil?
+
+    if user.valid_password?(params[:user][:password])
+      sign_in(:user, user)
+      user.ensure_authentication_token!
+      render json: {
+        authentication_token: user.authentication_token,
+        email: user.email,
+      }, status: :ok
+    else
+      invalid_login_attempt
+    end
+  end
+
 end
