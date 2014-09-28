ef90a80f4a2097cc8acedc7159179fdaeab309c3
Mark Miyashita <negativetwelve@gmail.com>
2014-09-27 21:35:03 -0700

    Annotate user and site models

--- watershed-rails/app/models/site.rb
+++ watershed-rails/app/models/site.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: sites
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  latitude    :decimal(10, 6)
+#  longitude   :decimal(10, 6)
+#  street      :text
+#  city        :string(255)
+#  state       :string(255)
+#  zip_code    :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Site < ActiveRecord::Base
 end
--- watershed-rails/spec/factories/sites.rb
+++ watershed-rails/spec/factories/sites.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: sites
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  latitude    :decimal(10, 6)
+#  longitude   :decimal(10, 6)
+#  street      :text
+#  city        :string(255)
+#  state       :string(255)
+#  zip_code    :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
