390311b8596e4c39cdaaf42381b71f9deb3778ec
Mark Miyashita <negativetwelve@gmail.com>
2014-09-27 21:53:42 -0700

    Fill in methods for create and update for sites

--- watershed-rails/app/controllers/api/v1/sites_controller.rb
+++ watershed-rails/app/controllers/api/v1/sites_controller.rb
@@ -1,13 +1,31 @@
 class Api::V1::SitesController < Api::V1::BaseController
-  load_and_authorize_resource :sites
+  load_and_authorize_resource :sites, param_method: :site_params
 
   def index
   end
 
   def create
+    if @site.save
+      # Success
+    else
+      # Error
+    end
   end
 
   def update
+    if @site.update(site_params)
+      # Success
+    else
+      # Error
+    end
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:name, :description, :street,
+                                 :city, :state, :zip_code,
+                                 :latitude, :longitude)
   end
 
 end
--- watershed-rails/db/seeds/development/001_sites.rb
+++ watershed-rails/db/seeds/development/001_sites.rb
@@ -1,12 +1,5 @@
 # Sites
 
-#  latitude    :decimal(10, 6)
-#  longitude   :decimal(10, 6)
-#  street      :text
-#  city        :string(255)
-#  state       :string(255)
-#  zip_code    :integer
-
 example_site = Site.where(
   name: "Example Site",
   description: "Shoreditch yr brunch, blog raw denim tofu American Apparel umami bicycle rights. Sartorial ennui asymmetrical pug, locavore squid Pitchfork master cleanse mustache. Cliche squid typewriter, DIY selvage aesthetic hoodie bitters distillery. Aesthetic cred Etsy, cardigan salvia tousled PBR&B Tumblr cray VHS lo-fi direct trade iPhone PBR Neutra. Chambray typewriter retro kale chips scenester hella. Whatever tousled wolf American Apparel paleo Blue Bottle. Deep v bitters bespoke before they sold out flexitarian Intelligentsia banh mi gluten-free kitsch Carles.",
