8eca11475a39584b13d9e87fb74666f806cb318e
Mark Miyashita <negativetwelve@gmail.com>
2014-09-27 21:18:05 -0700

    Add blank methods to the sites controller

--- watershed-rails/app/controllers/api/base_controller.rb
+++ watershed-rails/app/controllers/api/base_controller.rb
@@ -1,2 +1,15 @@
 class Api::BaseController < ApplicationController
+  before_filter :authenticate_user!
+  prepend_before_filter :get_authentication_token
+
+  respond_to :json
+
+  private
+
+  def get_authentication_token
+    if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
+      params[:auth_token] = auth_token
+    end
+  end
+
 end
---
+++ watershed-rails/app/controllers/api/v1/sites_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SitesController < Api::V1::BaseController
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end
