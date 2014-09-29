4bc02e125730c7f7b4bdc0c94dc69a93804cce05
Jordeen Chang <jordeenchang@gmail.com>
2014-09-28 23:00:25 -0700

    added rest of the colors

--- watershed-iOS/Watershed/Watershed/UIColor+WPColors.m
+++ watershed-iOS/Watershed/Watershed/UIColor+WPColors.m
@@ -13,6 +13,14 @@
 @implementation UIColor (WPColors)
 
 + (UIColor *)wp_darkBlue {
-    return [UIColor colorWithRed:207.0/255.0 green:47.0/255.0 blue:40.0/255.0 alpha:1];
+    return [UIColor colorWithRed:85.0/255.0 green:141.0/255.0 blue:173.0/255.0 alpha:1];
+}
+
++ (UIColor *)wp_blue {
+    return [UIColor colorWithRed:129.0/255.0 green:180.0/255.0 blue:222.0/255.0 alpha:1];
+}
+
++ (UIColor *)wp_lightBlue {
+    return [UIColor colorWithRed:188.0/255.0 green:218.0/255.0 blue:245.0/255.0 alpha:1];
 }
 @end
