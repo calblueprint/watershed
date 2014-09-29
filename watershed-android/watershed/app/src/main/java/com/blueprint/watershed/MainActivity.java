946a10a2918fae78d75066d90eebaf2585d3bd04
Mark Miyashita <negativetwelve@gmail.com>
2014-09-29 15:45:35 -0700

    Set up instance variables for the main activity

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/MainActivity.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/MainActivity.java
@@ -4,14 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.SharedPreferences;
 import com.blueprint.watershed.R;
 
 public class MainActivity extends Activity {
 
+    public  static final String PREFERENCES = "LOGIN_PREFERENCES";
+    private static final String TAG         = "MainActivity";
+
+    // Authenticating against our own server
+    public String authToken;
+    public String authEmail;
+
+    // For storing our credentials once we have successfully authenticated
+    SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initializeFragments();
+
         setContentView(R.layout.activity_main);
+
+        initializeNavigation();
+
+
     }
 
 
@@ -32,5 +50,13 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeFragments() {
+        // Initialize each of the fragments and set the current fragment
+    }
+
+    private void initializeNavigation() {
+        // Initialize the left drawer
     }
 }
