b3d8250b97ff4b3732815cdda2a79ec54c218a0b
Max Wolffe <max.alan.wolffe@gmail.com>
2014-09-28 22:32:11 -0700

    add Task java object

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TaskActivity.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TaskActivity.java
@@ -1,19 +1,18 @@
 package com.blueprint.watershed;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TaskActivity extends Activity {
+public class TaskActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
