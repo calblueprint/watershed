8cff93cba2cf9e5fe11339af6a6f7e91a905aa20
Jordeen Chang <jordeenchang@gmail.com>
2014-09-28 21:07:01 -0700

    created tasks list view controller

---
+++ watershed-iOS/Watershed/Watershed/WPTasksListViewController.m
@@ -0,0 +1,37 @@
+//
+//  WPTasksListViewController.m
+//  
+//
+//  Created by Jordeen Chang on 9/28/14.
+//
+//
+
+#import "WPTasksListViewController.h"
+
+@interface WPTasksListViewController ()
+
+@end
+
+@implementation WPTasksListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
