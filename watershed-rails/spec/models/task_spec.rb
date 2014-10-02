30bf5129e2b36e541f8871d5b53e725c3628bdc1
Max Wolffe <max.alan.wolffe@gmail.com>
2014-10-01 21:15:26 -0700

    add task controller

---
+++ watershed-rails/spec/models/task_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  site_id     :integer
+#  assigner_id :integer
+#  assignee_id :integer
+#  complete    :boolean
+#  due_date    :datetime
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+require 'spec_helper'
+
+describe Task do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
