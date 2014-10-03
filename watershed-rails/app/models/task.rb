f86c982d1a7475524366a019d1523196dc4a386b
Mark Miyashita <negativetwelve@gmail.com>
2014-10-03 13:05:46 -0700

    Use double quotes and add relationships for sites

--- watershed-rails/app/models/task.rb
+++ watershed-rails/app/models/task.rb
@@ -15,6 +15,6 @@
 #
 
 class Task < ActiveRecord::Base
-  belongs_to :assigner, class_name: 'User', foreign_key: 'assigner_id'
-  belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id'
+  belongs_to :assigner, class_name: "User", foreign_key: "assigner_id"
+  belongs_to :assignee, class_name: "User", foreign_key: "assignee_id"
 end
