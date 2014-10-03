0dc4798f7824f1674ed1be84bcb7b1d6d7edcf97
Mark Miyashita <negativetwelve@gmail.com>
2014-10-03 13:06:10 -0700

    Clean up field report model

--- watershed-rails/app/models/field_report.rb
+++ watershed-rails/app/models/field_report.rb
@@ -1,10 +1,11 @@
 class FieldReport < ActiveRecord::Base
   belongs_to :user
-  validates :user_id, presence: true
   belongs_to :mini_site
-  validates :mini_site_id, presence: true
-  validates :health_rating, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 5 }
 
-  default_scope -> { order('created_at DESC') }
+  validates :user_id, presence: true
+  validates :mini_site_id, presence: true
+  validates :health_rating, numericality: { less_than_or_equal_to: 5 }
+
+  default_scope -> { order("created_at DESC") }
 
 end
