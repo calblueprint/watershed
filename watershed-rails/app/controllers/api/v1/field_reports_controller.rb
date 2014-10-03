f9c35f86998c216a15023568106f106a6b3da354
Andrew Millman <andrewmillman35@gmail.com>
2014-10-02 17:52:18 -0700

    Create FieldReport Controller

---
+++ watershed-rails/app/controllers/api/v1/field_reports_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::FieldReportsController < Api::V1::BaseController
+  load_and_authorize_resource param_method: :field_report_params
+
+def index
+    render json: @field_reports
+  end
+
+  def create
+    if @field_report.save
+      render json: @field_report
+    else
+      render json: { errors: @field_report.errors }, status: 422
+    end
+  end
+
+  def update
+    if @field_report.update(field_report_params)
+      render json: @field_report
+    else
+      render json: { errors: @field_report.errors }, status: 422
+    end
+  end
+
+  private
+
+  def field_report_params
+    params.require(:field_report).permit(:user_id, :mini_site_id,
+      :description, :health_rating, :urgent)
+  end
+end
