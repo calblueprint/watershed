f0a684a608320c0b6ce25ea81ae846161e79ab22
Mark Miyashita <negativetwelve@gmail.com>
2014-10-03 13:07:19 -0700

    Annotate field report model

--- watershed-rails/app/models/field_report.rb
+++ watershed-rails/app/models/field_report.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: field_reports
+#
+#  id            :integer          not null, primary key
+#  user_id       :integer
+#  mini_site_id  :integer
+#  description   :text
+#  health_rating :integer
+#  urgent        :boolean          default(FALSE)
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class FieldReport < ActiveRecord::Base
   belongs_to :user
   belongs_to :mini_site
