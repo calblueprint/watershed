10b9351c20499812b62e66ed4e90aa83278e244a
Mark Miyashita <negativetwelve@gmail.com>
2014-10-04 20:08:47 -0700

    Add cover photo class

---
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/CoverPhotoView.java
@@ -0,0 +1,16 @@
+package com.blueprint.watershed;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Mark Miyashita on 10/4/14.
+ */
+public class CoverPhotoView extends View {
+
+    public CoverPhotoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
