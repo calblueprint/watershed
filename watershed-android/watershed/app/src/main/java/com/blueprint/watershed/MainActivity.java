59e02a216d620a01853c0dbdcc997f9ec2e184f8
Mark Miyashita <negativetwelve@gmail.com>
2014-10-04 21:03:29 -0700

    Got rid of the action bar and title woop

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/MainActivity.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/MainActivity.java
@@ -65,26 +65,6 @@ public class MainActivity extends ActionBarActivity
         mTitle = getTitle();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public void replaceFragment(Fragment newFragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction ft = fragmentManager.beginTransaction();
