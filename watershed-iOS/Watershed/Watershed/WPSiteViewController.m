d8436b2d2dcf5c302c77b35a48f6402254498672
Andrew Millman <andrewmillman35@gmail.com>
2014-10-05 20:57:34 -0700

    Add SiteViewController

---
+++ watershed-iOS/Watershed/Watershed/WPSiteViewController.m
@@ -0,0 +1,48 @@
+//
+//  WPSiteViewController.m
+//  Watershed
+//
+//  Created by Andrew on 10/5/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPSiteViewController.h"
+#import "WPSiteView.h"
+
+@interface WPSiteViewController ()
+
+@end
+
+@implementation WPSiteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)loadView
+{
+    self.view = [[WPSiteView alloc] init];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
