ed221136aa1ae5e76e3d87731078350c7d28bfbf
Andrew Millman <andrewmillman35@gmail.com>
2014-10-06 11:30:52 -0700

    Add initial logic for blurring the cover photo

--- watershed-iOS/Watershed/Watershed/WPSiteView.m
+++ watershed-iOS/Watershed/Watershed/WPSiteView.m
@@ -15,6 +15,9 @@
 @interface WPSiteView ()
 
 @property (nonatomic) UIImageView *coverPhotoView;
+@property (nonatomic) UIImage *originalCoverPhoto;
+@property (nonatomic) NSMutableArray *coverPhotoArray;
+@property (nonatomic) NSInteger blurRadius;
 
 @end
 
@@ -24,6 +27,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        self.backgroundColor =[UIColor whiteColor];
         [self createSubviews];
         [self setUpActions];
         [self updateConstraints];
@@ -36,26 +40,76 @@
 - (void)createSubviews {
 
     UIImage *coverPhoto = [UIImage imageNamed:@"SampleCoverPhoto"];
+    _originalCoverPhoto = coverPhoto;
+    
     UIImageView *coverPhotoView = [[UIImageView alloc] initWithImage:coverPhoto];
     [coverPhotoView setContentMode:UIViewContentModeScaleAspectFill];
+    [coverPhotoView setClipsToBounds:YES];
     _coverPhotoView = coverPhotoView;
+    [self addSubview:coverPhotoView];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        for (int i = 0; i <= 40; i++) {
+            UIImage *image = self.originalCoverPhoto;
+            image = [image applyBlurWithRadius:i
+                                     tintColor:[UIColor colorWithRed:0
+                                                               green:0
+                                                                blue:0
+                                                               alpha:(i * 0.15 / 40)]
+                         saturationDeltaFactor:1
+                                     maskImage:nil];
+            [self.coverPhotoArray addObject:image];
+            NSLog(@"%d", i);
+        }
+    });
     
     
 }
 
 - (void)setUpActions {
-    // Here is where you set up buttons taps and gesture recognizers.
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                          action:@selector(pan:)];
+    [self addGestureRecognizer:pan];
 }
 
 - (void)updateConstraints {
     
     [self.coverPhotoView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.equalTo(@200);
+        make.height.equalTo(@184);
+        make.top.equalTo(@0);
         make.leading.equalTo(@0);
         make.trailing.equalTo(@0);
     }];
     
     [super updateConstraints];
+}
+
+- (void)pan:(UIPanGestureRecognizer *)sender {
+    if ((sender.state == UIGestureRecognizerStateChanged) ||
+        (sender.state == UIGestureRecognizerStateEnded)) {
+        CGPoint trans = [sender translationInView:self];
+        
+        self.blurRadius -= trans.y;
+        if (self.blurRadius < 0) {self.blurRadius = 0;}
+        if (self.blurRadius > 40) {self.blurRadius = 40;}
+        
+        [self.coverPhotoView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.height.equalTo([[NSNumber alloc] initWithFloat:184.0 - self.blurRadius * 3 ]);
+        }];
+        
+        [super updateConstraints];
+        
+        [self.coverPhotoView setImage:self.coverPhotoArray[self.blurRadius]];
+        [sender setTranslation:CGPointZero inView:self];
+    }
+}
+
+- (NSMutableArray *)coverPhotoArray
+{
+    if (!_coverPhotoArray) {
+        _coverPhotoArray = [[NSMutableArray alloc] init];
+    }
+    return _coverPhotoArray;
 }
 
 @end
