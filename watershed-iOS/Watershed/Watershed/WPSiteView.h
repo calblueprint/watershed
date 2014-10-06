cbcfe1589c2562d6e23359cb1cbd12d4dbd348ac
Andrew Millman <andrewmillman35@gmail.com>
2014-10-05 20:52:33 -0700

    Add view files for Site

---
+++ watershed-iOS/Watershed/Watershed/WPSiteView.h
@@ -0,0 +1,13 @@
+//
+//  WPSiteView.h
+//  Watershed
+//
+//  Created by Andrew on 10/5/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPSiteView : UIView
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPSiteView.m
@@ -0,0 +1,31 @@
+//
+//  WPSiteView.m
+//  Watershed
+//
+//  Created by Andrew on 10/5/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPSiteView.h"
+
+@implementation WPSiteView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
