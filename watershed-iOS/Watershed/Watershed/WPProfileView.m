930cbfb07937520c154e000df39481a857022dba
Melissa Huang <melissahuang10@gmail.com>
2014-10-19 20:41:01 -0700

    Add Font Awesome pod

--- watershed-iOS/Watershed/Watershed/WPProfileView.m
+++ watershed-iOS/Watershed/Watershed/WPProfileView.m
@@ -10,6 +10,7 @@
 #import "Masonry.h"
 #import "UIColor+WPColors.h"
 #import "UIView+WPExtensions.h"
+#import "FontAwesomeKit/FontAwesomeKit.h"
 
 #import <QuartzCore/QuartzCore.h>
 
@@ -17,10 +18,16 @@
 
 @property (nonatomic) UIView *profilePictureView;
 @property (nonatomic) UILabel *nameLabel;
-@property (nonatomic) UIView *tasksView;
-@property (nonatomic) UIView *tasksLabel;
-@property (nonatomic) UIView *tasksNumber;
-@property (nonatomic) UIView *sitesView;
+//@property (nonatomic) UIView *tasksView;
+//@property (nonatomic) UIView *tasksLabel;
+//@property (nonatomic) UIView *tasksNumber;
+//@property (nonatomic) UIView *sitesView;
+@property (nonatomic) UIView *locationView;
+@property (nonatomic) UIView *locationIconView;
+@property (nonatomic) UILabel *locationLabel;
+@property (nonatomic) UIImageView *mailIconImageView;
+
+
 
 @end
 
@@ -31,7 +38,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor wp_blue];
+        self.backgroundColor = [UIColor whiteColor];
         [self createSubviews];
         //[self setUpActions];
         [self updateConstraints];
@@ -46,10 +53,10 @@
     CGPoint saveCenter = roundedView.center;
     CGRect newFrame = CGRectMake(roundedView.frame.origin.x, roundedView.frame.origin.y, newSize, newSize);
     roundedView.frame = newFrame;
-    roundedView.layer.borderWidth = 2.0f;
-    roundedView.layer.borderColor = [UIColor whiteColor].CGColor;
+    roundedView.layer.borderWidth = 1.0f;
+    roundedView.layer.borderColor = [UIColor blackColor].CGColor;
 
-    roundedView.layer.cornerRadius = 75/2;
+    roundedView.layer.cornerRadius = 65/2;
     roundedView.center = saveCenter;
 }
 
@@ -58,7 +65,7 @@
     UIImageView *profilePictureView = [[UIImageView alloc] init];
     profilePictureView.contentMode = UIViewContentModeScaleAspectFit;
     profilePictureView.clipsToBounds = YES;
-    [self setRoundedView:profilePictureView toDiameter:20];
+    [self setRoundedView:profilePictureView toDiameter:10];
     [profilePictureView setImage:[UIImage imageNamed:@"max.png"]];
     _profilePictureView = profilePictureView;
     [self addSubview:profilePictureView];
@@ -66,35 +73,56 @@
 
     UILabel *nameLabel = [[UILabel alloc] init];
     nameLabel.text = @"Max Wolffe";
-    nameLabel.textColor = [UIColor whiteColor];
+    nameLabel.textColor = [UIColor blackColor];
     _nameLabel = nameLabel;
     [self addSubview:nameLabel];
     
+    UIView *locationView = [[UIView alloc] init];
+    [self addSubview:locationView];
     
+    FAKIonIcons *locationIcon = [FAKIonIcons ios7LocationOutlineIconWithSize:20];
+    _locationImageView = [[UIImageView alloc] init];
+    [_mailIconImageView setImage:[mailIcon imageWithSize:CGSizeMake(15, 15)]];
     
-    UIView *tasksView = [[UIView alloc] init];
-    tasksView.layer.borderWidth = 0.5f;
-    tasksView.layer.borderColor = [UIColor grayColor].CGColor;
-    _tasksView = tasksView;
-    [self addSubview:tasksView];
+    [self addSubview:_mailIconImageView];
     
-    UILabel *tasksNumber = [[UILabel alloc] init];
-    tasksNumber.text = @"3";
-    tasksNumber.textColor = [UIColor whiteColor];
-    _tasksNumber = tasksNumber;
-    [tasksView addSubview:tasksNumber];
+    UILabel *locationLabel = [[UILabel alloc] init];
+    locationLabel.text = @"123 Cloyne Way Berkeley, CA 94709";
+    locationLabel.textColor = [UIColor blackColor];
+    locationLabel.font = [UIFont fontWithName:@"Helvetica" size:10];
+    _locationLabel = locationLabel;
+//    [self addSubview:locationLabel];
+    [locationView addSubview:locationLabel];
     
-    UILabel *tasksLabel = [[UILabel alloc] init];
-    tasksLabel.text = @"Tasks";
-    tasksLabel.textColor = [UIColor whiteColor];
-    _tasksLabel = tasksLabel;
-    [tasksView addSubview:tasksLabel];
+    FAKIonIcons *mailIcon = [FAKIonIcons ios7EmailOutlineIconWithSize:20];
+    _mailIconImageView = [[UIImageView alloc] init];
+    [_mailIconImageView setImage:[mailIcon imageWithSize:CGSizeMake(15, 15)]];
+
+    [self addSubview:_mailIconImageView];
     
-    UIView *sitesView = [[UIView alloc] init];
-    sitesView.layer.borderWidth = 0.5f;
-    sitesView.layer.borderColor = [UIColor grayColor].CGColor;
-    _sitesView = sitesView;
-    [self addSubview:sitesView];
+//    UIView *tasksView = [[UIView alloc] init];
+//    tasksView.layer.borderWidth = 0.5f;
+//    tasksView.layer.borderColor = [UIColor grayColor].CGColor;
+//    _tasksView = tasksView;
+//    [self addSubview:tasksView];
+//    
+//    UILabel *tasksNumber = [[UILabel alloc] init];
+//    tasksNumber.text = @"3";
+//    tasksNumber.textColor = [UIColor whiteColor];
+//    _tasksNumber = tasksNumber;
+//    [tasksView addSubview:tasksNumber];
+//    
+//    UILabel *tasksLabel = [[UILabel alloc] init];
+//    tasksLabel.text = @"Tasks";
+//    tasksLabel.textColor = [UIColor whiteColor];
+//    _tasksLabel = tasksLabel;
+//    [tasksView addSubview:tasksLabel];
+//    
+//    UIView *sitesView = [[UIView alloc] init];
+//    sitesView.layer.borderWidth = 0.5f;
+//    sitesView.layer.borderColor = [UIColor grayColor].CGColor;
+//    _sitesView = sitesView;
+//    [self addSubview:sitesView];
     
 }
 
@@ -103,8 +131,8 @@
     [self.profilePictureView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(@100);
         make.leading.equalTo(@40);
-        make.height.equalTo(@75);
-        make.width.equalTo(@75);
+        make.height.equalTo(@65);
+        make.width.equalTo(@65);
     }];
     
     [self.nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -114,31 +142,59 @@
         make.width.equalTo(@100);
     }];
     
-    [self.tasksView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(@200);
-        make.leading.equalTo(@0);
-        make.height.equalTo(@40);
-        make.width.equalTo(@160);
+    [self.locationLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@220);
+        make.leading.equalTo(@95);
+        make.height.equalTo(@20);
+        make.width.equalTo(@200);
     }];
-    [self.tasksLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(@10);
-        make.leading.equalTo(@30);
-        make.height.equalTo(@10);
-        make.width.equalTo(@20);
-    }];
-    [self.tasksNumber mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(@5);
-        make.leading.equalTo(@30);
-        make.height.equalTo(@10);
+
+    [self.locationIconView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@215);
+        make.leading.equalTo(@55);
+        make.height.equalTo(@20);
         make.width.equalTo(@20);
     }];
     
-    [self.sitesView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(@200);
-        make.trailing.equalTo(@0);
-        make.height.equalTo(@40);
-        make.width.equalTo(@160);
+    [self.locationView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@110);
+        make.leading.equalTo(@125);
+        make.height.equalTo(@50);
+        make.width.equalTo(@200);
     }];
+    
+    [self.mailIconImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@285);
+        make.leading.equalTo(@55);
+        make.height.equalTo(@20);
+        make.width.equalTo(@20);
+    }];
+    
+//    [self.tasksView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@200);
+//        make.leading.equalTo(@0);
+//        make.height.equalTo(@40);
+//        make.width.equalTo(@160);
+//    }];
+//    [self.tasksLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@10);
+//        make.leading.equalTo(@30);
+//        make.height.equalTo(@10);
+//        make.width.equalTo(@20);
+//    }];
+//    [self.tasksNumber mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@5);
+//        make.leading.equalTo(@30);
+//        make.height.equalTo(@10);
+//        make.width.equalTo(@20);
+//    }];
+//    
+//    [self.sitesView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(@200);
+//        make.trailing.equalTo(@0);
+//        make.height.equalTo(@40);
+//        make.width.equalTo(@160);
+//    }];
     
     [super updateConstraints];
 }
