46e85a3c8ffa7666017e44de3e227b5650b48ee9
Max Wolffe <max.alan.wolffe@gmail.com>
2014-10-19 21:59:43 -0700

    add tabs, lost reside menu

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TabsPagerAdapter.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TabsPagerAdapter.java
@@ -21,10 +21,13 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
 
         switch (index) {
             case 0:
+                return new TaskFragment();
                 // Top Rated fragment activity
             case 1:
+                return new TaskFragment();
                 // Games fragment activity
             case 2:
+                return new TaskFragment();
                 // Movies fragment activity
         }
 
