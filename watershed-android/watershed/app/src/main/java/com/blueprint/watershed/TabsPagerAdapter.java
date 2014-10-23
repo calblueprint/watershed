49dfe3bbcbdb32eb4c9b427289eb506b3dd31cb0
Max Wolffe <max.alan.wolffe@gmail.com>
2014-10-22 22:09:03 -0700

    Fixed Tabs

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TabsPagerAdapter.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TabsPagerAdapter.java
@@ -32,7 +32,7 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // get item count - equal to number of tabs
-        return 3;
+        return 2;
     }
 
 }
