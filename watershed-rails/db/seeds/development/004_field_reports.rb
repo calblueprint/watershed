17a06c6a7a9a119a8b7c60c20379db1ed08a896f
Andrew Millman <andrewmillman35@gmail.com>
2014-10-24 23:08:10 -0700

    Finish creating seed data for field reports

--- watershed-rails/db/seeds/development/004_field_reports.rb
+++ watershed-rails/db/seeds/development/004_field_reports.rb
@@ -1,13 +1,18 @@
 # Field Reports
 
-(0..20).each do |number|
-  example_field_report = FieldReport.where(
-    user_id:
-    mini_site_id:
-    description:
-    health_rating:
-    urgent:
-  ).first_or_create
+mini_site_count = MiniSite.count
+user_count = User.count
 
-  puts "Created Field Report: #{example_field_report.urgent}"
+(0..mini_site_count).each do |mini_site_number|
+  (0..2).each do |report_number|
+    example_field_report = FieldReport.where(
+      user_id: rand(0..user_count) + 1,
+      mini_site_id: mini_site_number + 1
+      description: "Oh wow, this tree sucks so much",
+      health_rating: rand(0..5) + 1,
+      urgent: [true, false][report_number]
+    ).first_or_create
+    puts "Created Field Report: Report #{report_number} at Mini Site #{mini_site_number}.
+          Created by User #{example_field_report.user_id}"
+  end
 end
