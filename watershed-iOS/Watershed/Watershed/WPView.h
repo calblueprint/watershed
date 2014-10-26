ee4b3823ee268324aa7c9a76ce6d14e328c491a8
Andrew Millman <andrewmillman35@gmail.com>
2014-10-25 22:36:58 -0700

    Add superview UIView for all view controller views to implement

---
+++ watershed-iOS/Watershed/Watershed/WPView.h
@@ -0,0 +1,15 @@
+//
+//  WPView.h
+//  Watershed
+//
+//  Created by Andrew on 10/25/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPView : UIView
+
+- (instancetype)initWithVisibleNavbar:(BOOL)visible;
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPView.m
@@ -0,0 +1,54 @@
+//
+//  WPView.m
+//  Watershed
+//
+//  Created by Andrew on 10/25/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPView.h"
+#import "UIView+WPExtensions.h"
+#import "UIColor+WPColors.h"
+#import "Masonry.h"
+
+@interface WPView ()
+
+@property UIView *navbarView;
+@property BOOL navbarVisible;
+
+@end
+
+@implementation WPView
+
+- (instancetype)initWithVisibleNavbar:(BOOL)visible {
+    self = [super init];
+    if (self) {
+        
+        _navbarVisible = visible;
+        if (visible) {
+            _navbarView = [({
+                UIView *navbarView = [[UIView alloc] init];
+                navbarView.backgroundColor = [UIColor wp_blue];
+                navbarView;
+            }) wp_addToSuperview:self];
+        }
+        
+        [self setNeedsUpdateConstraints];
+    }
+    return self;
+
+}
+
+- (void)updateConstraints {
+    if (self.navbarVisible) {
+        [self.navbarView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.height.equalTo(@64);
+            make.leading.equalTo(@0);
+            make.trailing.equalTo(@0);
+        }];
+    }
+    
+    [super updateConstraints];
+}
+
+@end
