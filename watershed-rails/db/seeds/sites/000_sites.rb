d82e77c64f2d30e87b12bb2447e3381201b2df4c
Mark Miyashita <negativetwelve@gmail.com>
2014-10-26 20:26:37 -0700

    Split the row and print out the site name for now

--- watershed-rails/db/seeds/sites/000_sites.rb
+++ watershed-rails/db/seeds/sites/000_sites.rb
@@ -3,5 +3,6 @@ require "csv"
 csv_file = File.join(Rails.root, "db", "seeds", "sites", "import.csv")
 
 CSV.foreach(csv_file, headers: true) do |row|
-  puts row
+  site_name, date_planted, address_number, street, common_name_1, common_name_2, common_name_3, species_name_1, species_name_2, species_name_3, latitude, longitude = row.to_a
+  puts site_name
 end
