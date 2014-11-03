7ab7929cfcb910db309df1097bcd030e7ce67f23
Max Wolffe <max.alan.wolffe@gmail.com>
2014-11-02 21:17:06 -0800

    add request

---
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/BaseRequest.java
@@ -0,0 +1,40 @@
+package com.blueprint.watershed;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by maxwolffe on 11/2/14.
+ */
+public abstract class BaseRequest extends Request{
+    private RequestHandler singletonRequestHandler;
+    private Map<String,String> params;
+
+    public BaseRequest(int method, String url, JSONObject jsonRequest,
+                       Response.Listener<JSONArray> listener, Response.ErrorListener errorListener){
+        super(method, url, jsonRequest, listener, errorListener);
+    }
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        Map<String, String> headers = super.getHeaders();
+
+        if (headers == null
+                || headers.equals(Collections.emptyMap())) {
+            headers = new HashMap<String, String>();
+        }
+        return headers;
+    }
+}
