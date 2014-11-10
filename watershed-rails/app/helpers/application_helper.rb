ba1f8cef2152406ced82da3f687d84b7d5be4389
Mark Miyashita <negativetwelve@gmail.com>
2014-11-09 19:21:23 -0800

    Add helper to get the correct json for the object

--- watershed-rails/app/helpers/application_helper.rb
+++ watershed-rails/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def json_for(target, options = {})
+    options[:scope] ||= self
+    options[:url_options] ||= url_options
+    target.active_model_serializer.new(target, options).to_json
+  end
+
 end
