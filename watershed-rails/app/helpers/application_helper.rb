572b7ed96e5b78ba042a10f97238ab071431ea22
Mark Miyashita <negativetwelve@gmail.com>
2014-11-09 19:25:40 -0800

    Override active record instead of using a helper

--- watershed-rails/app/helpers/application_helper.rb
+++ watershed-rails/app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-
-  def json_for(target, options = {})
-    options[:scope] ||= self
-    options[:url_options] ||= url_options
-    target.active_model_serializer.new(target, options).to_json
-  end
-
 end
