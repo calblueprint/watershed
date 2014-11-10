ea981ba1a0ba48e2ed547194391435b20695cddd
Melissa Huang <melissahuang10@gmail.com>
2014-11-09 19:42:38 -0800

    About VC

---
+++ watershed-iOS/Watershed/Watershed/WPAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPAboutViewController.h
+//  Watershed
+//
+//  Created by Melissa Huang on 11/9/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPAboutViewController : UIViewController
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPAboutViewController.m
@@ -0,0 +1,13 @@
+//
+//  WPAboutViewController.m
+//  Watershed
+//
+//  Created by Melissa Huang on 11/9/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPAboutViewController.h"
+
+@implementation WPAboutViewController
+
+@end
