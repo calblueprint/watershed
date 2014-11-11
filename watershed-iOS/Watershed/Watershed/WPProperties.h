e5b51141917f7b9af536f04223a9ce3aa62a1da3
Jordeen Chang <jordeenchang@gmail.com>
2014-11-10 20:43:04 -0800

    add field report view still ugly but not THAT ugly

--- watershed-iOS/Watershed/Watershed/WPProperties.h
+++ watershed-iOS/Watershed/Watershed/WPProperties.h
@@ -15,7 +15,7 @@ static const CGFloat topMargin = 64;
 
 static const CGFloat wpCornerRadius = 3;
 
-static const CGFloat wpBorderWidth = 2.0f;
+static const CGFloat wpBorderWidth = 1.0f;
 
 static const CGFloat wpButtonWidth = 250;
 
