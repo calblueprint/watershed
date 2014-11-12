eb95e319502d37ffa7d2e85d5332459bbb2bff22
Max Wolffe <max.alan.wolffe@gmail.com>
2014-11-12 13:40:14 -0800

    add getTasksRequest, just adapted Marks code, do we want to make request specific classes?

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TaskFragment.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/TaskFragment.java
@@ -14,7 +14,17 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 public class TaskFragment extends ListFragment {
 
@@ -22,6 +32,8 @@ public class TaskFragment extends ListFragment {
     private ListView listView1;
     private Task mTaskList[];
     private MainActivity parentActivity;
+    private TaskAdapter mTaskAdapter;
+    private RequestHandler mRequestHandler;
 
 
     public static TaskFragment newInstance(int option) {
@@ -38,6 +50,7 @@ public class TaskFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mRequestHandler = RequestHandler.getInstance(getActivity().getApplicationContext());
         parentActivity = (MainActivity)getActivity();
         if (getArguments() != null) {
             int option = getArguments().getInt("option");
@@ -108,8 +121,36 @@ public class TaskFragment extends ListFragment {
     }
 
     private void getTasksRequest(){
+        HashMap<String, JSONObject> params = new HashMap<String, JSONObject>();
 
+        // TODO(mark): URL and mapper should be generalized
+        String url = "https://intense-reaches-1457.herokuapp.com/api/v1/tasks";
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+
+        BaseRequest request = new BaseRequest(Request.Method.GET, url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        try {
+                            String sitesJson = jsonObject.get("tasks").toString();
+                            ArrayList<Task> tasks = mapper.readValue(sitesJson, new TypeReference<ArrayList<Task>>() {
+                            });
+                            setTasks(tasks);
+                            Log.i("task count", Integer.toString(tasks.size()));
+                            mTaskAdapter.notifyDataSetChanged();
+                            Log.e("response", jsonObject.toString());
+                        } catch (Exception e) {
+                            Log.e("Json exception", "in site list fragment" + e.toString());
+                        }
+                    }
+                }
+                , getActivity()){};
+
+        mRequestHandler.getRequestQueue().add(request);
     }
+
+    private void setTasks(ArrayList<Task> tasks){};
 
     public interface OnFragmentInteractionListener {
         public void onFragmentInteraction(Uri uri);
