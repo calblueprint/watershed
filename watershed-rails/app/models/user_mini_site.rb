2381543b34e2808c60f1ea8ea8a83b0fdc22c346
Mark Miyashita <negativetwelve@gmail.com>
2014-11-12 21:51:59 -0800

    Reannotate user_mini_sites class

--- watershed-rails/app/models/user_mini_site.rb
+++ watershed-rails/app/models/user_mini_site.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: user_mini_sites
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  mini_site_id :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class UserMiniSite < ActiveRecord::Base
   belongs_to :user
   belongs_to :mini_site
