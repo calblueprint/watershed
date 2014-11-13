90b9f0d77d8b4499b576fdac28246f904a6ea157
Mark Miyashita <negativetwelve@gmail.com>
2014-11-12 21:23:55 -0800

    Add assignee and assigner to task serializer

--- watershed-rails/app/serializers/task_serializer.rb
+++ watershed-rails/app/serializers/task_serializer.rb
@@ -2,6 +2,6 @@ class TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :site_id,
              :complete, :due_date
 
-  #belongs_to :assignee
-  #belongs_to :assigner
+  has_one :assignee
+  has_one :assigner
 end
