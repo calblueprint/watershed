b6aa8c81c3311f13b17381944438b0c6082b2b98
Mark Miyashita <negativetwelve@gmail.com>
2014-11-12 21:55:52 -0800

    Send back users for mini_sites and mini_sites for users

--- watershed-rails/config/routes.rb
+++ watershed-rails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       devise_for :users
+      resources :users, only: [:index, :show, :create, :update]
 
       resources :sites, only: [:index, :show, :create, :update]
       resources :tasks, only: [:index, :show, :create, :update]
