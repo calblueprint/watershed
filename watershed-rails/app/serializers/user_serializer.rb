37d01dff02f289c12b78b51023bb2fc44c45be77
Mark Miyashita <negativetwelve@gmail.com>
2014-11-12 22:48:31 -0800

    Prevent infinite loops from users to mini sites json rendering

--- watershed-rails/app/serializers/user_serializer.rb
+++ watershed-rails/app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :role
 
   has_many :tasks
-  has_many :mini_sites
+  has_many :mini_sites, embed: :ids, include: false
   has_many :field_reports
 end
