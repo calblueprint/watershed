627fb1b0dea55c772e92de4e8885f8a8c92f4a94
Andrew Millman <andrewmillman35@gmail.com>
2014-11-14 17:08:10 -0800

    Resort to default search icon

--- watershed-iOS/Watershed/Watershed/WPSitesTableViewController.m
+++ watershed-iOS/Watershed/Watershed/WPSitesTableViewController.m
@@ -107,9 +107,11 @@ static NSString *cellIdentifier = @"SiteCell";
 #pragma mark - Search / Search Delegate Methods
 
 - (void)setUpSearchBar {
-    FAKFontAwesome *searchIcon = [FAKFontAwesome searchIconWithSize:18];
-    UIImage *searchImage = [searchIcon imageWithSize:CGSizeMake(20, 20)];
-    UIBarButtonItem *searchButtonItem = [[UIBarButtonItem alloc] initWithImage:searchImage style:UIBarButtonItemStylePlain target:self action:@selector(openSearch)];
+
+    UIBarButtonItem *searchButtonItem = [[UIBarButtonItem alloc]
+                                         initWithBarButtonSystemItem:UIBarButtonSystemItemSearch
+                                         target:self
+                                         action:@selector(openSearch) ];
     searchButtonItem.tintColor = [UIColor whiteColor];
     self.navigationItem.rightBarButtonItem = searchButtonItem;
     
