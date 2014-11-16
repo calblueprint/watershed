609b9019803b7f5f5bd3bd78c010cf39f1d2c6fe
Andrew Millman <andrewmillman35@gmail.com>
2014-11-15 20:10:45 -0800

    Add properties to site model

--- watershed-iOS/Watershed/Watershed/WPSite.h
+++ watershed-iOS/Watershed/Watershed/WPSite.h
@@ -10,4 +10,12 @@
 
 @interface WPSite : NSObject
 
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *info;
+@property (nonatomic) CGPoint *position;
+@property (nonatomic) NSString *street;
+@property (nonatomic) NSString *city;
+@property (nonatomic) NSString *state;
+@property (nonatomic) NSNumber *zipCode;
+
 @end
