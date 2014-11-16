d1e1dda854d4196157f2b3576a0cb5c1b8f5fb82
Andrew Millman <andrewmillman35@gmail.com>
2014-11-15 20:22:41 -0800

    Add FieldReport model

---
+++ watershed-iOS/Watershed/Watershed/WPFieldReport.h
@@ -0,0 +1,13 @@
+//
+//  WPFieldReport.h
+//  Watershed
+//
+//  Created by Andrew on 11/15/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPFieldReport : NSObject
+
+@end
