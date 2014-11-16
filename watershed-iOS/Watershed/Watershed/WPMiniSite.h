3a6ee09559cf60ac727c5743aad49e309c748b84
Andrew Millman <andrewmillman35@gmail.com>
2014-11-15 20:13:05 -0800

    Add MiniSite model

---
+++ watershed-iOS/Watershed/Watershed/WPMiniSite.h
@@ -0,0 +1,13 @@
+//
+//  WPMiniSite.h
+//  Watershed
+//
+//  Created by Andrew on 11/15/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPMiniSite : NSObject
+
+@end
