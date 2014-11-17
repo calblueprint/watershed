08e6808b3d2cf5c85413992fdf3357e22c64998a
Mark Miyashita <negativetwelve@gmail.com>
2014-11-16 19:34:25 -0800

    Add individual site request

---
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Networking/SiteRequest.java
@@ -0,0 +1,41 @@
+package com.blueprint.watershed.Networking;
+
+import android.app.Activity;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.blueprint.watershed.Sites.Site;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by mark on 11/16/14.
+ */
+public class SiteRequest extends BaseRequest {
+
+    public SiteRequest(final Activity activity, HashMap<String, JSONObject> params, final Response.Listener<Site> listener) {
+        super(Request.Method.GET, makeURL("site"), new JSONObject(params),
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject jsonObject) {
+                    try {
+                        String sitesJson = jsonObject.get("site").toString();
+                        ObjectMapper mapper = getNetworkManager(activity.getApplicationContext()).getObjectMapper();
+                        Site site = mapper.readValue(sitesJson, new TypeReference<Site>() {
+                        });
+                        listener.onResponse(site);
+                    } catch (Exception e) {
+                        Log.e("Json exception", e.toString());
+                    }
+                }
+
+            }, activity);
+    }
+
+}
