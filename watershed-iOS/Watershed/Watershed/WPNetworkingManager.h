012ae5ee2f14c83b69528a690089c56723271d1b
Andrew Millman <andrewmillman35@gmail.com>
2014-11-16 20:20:08 -0800

    Add base networking class

---
+++ watershed-iOS/Watershed/Watershed/WPNetworkingManager.h
@@ -0,0 +1,13 @@
+//
+//  WPNetworkingManager.h
+//  Watershed
+//
+//  Created by Andrew Millman on 11/16/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPNetworkingManager : NSObject
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPNetworkingManager.m
@@ -0,0 +1,13 @@
+//
+//  WPNetworkingManager.m
+//  Watershed
+//
+//  Created by Andrew Millman on 11/16/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPNetworkingManager.h"
+
+@implementation WPNetworkingManager
+
+@end
