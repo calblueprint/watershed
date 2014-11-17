4b63550ba476e065a2c5ba8c4d52e5c70757537f
Andrew Millman <andrewmillman35@gmail.com>
2014-11-16 20:44:55 -0800

    Add singleton methods for networking manager class

--- watershed-iOS/Watershed/Watershed/WPNetworkingManager.m
+++ watershed-iOS/Watershed/Watershed/WPNetworkingManager.m
@@ -7,7 +7,40 @@
 //
 
 #import "WPNetworkingManager.h"
+#import "WPAppDelegate.h"
+
+@interface WPNetworkingManager ()
+@property (nonatomic) AFHTTPRequestOperationManager *requestManager;
+@property (nonatomic) WPAppDelegate *appDelegate;
+@end
 
 @implementation WPNetworkingManager
+
+#pragma mark - Singleton Methods
+
++ (id)sharedManager {
+    static WPNetworkingManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+#pragma mark - Lazy Instantiation
+
+- (AFHTTPRequestOperationManager *)requestManager {
+    if (!_requestManager) {
+        _requestManager = self.appDelegate.getAFManager;
+    }
+    return _requestManager;
+}
+
+- (WPAppDelegate *)appDelegate {
+    if (!_appDelegate) {
+        _appDelegate = [WPAppDelegate instance];
+    }
+    return _appDelegate;
+}
 
 @end
