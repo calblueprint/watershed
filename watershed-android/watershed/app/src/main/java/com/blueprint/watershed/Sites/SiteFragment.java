c6b9e2adcaffbb34638e0402a2c6c41653f03922
Mark Miyashita <negativetwelve@gmail.com>
2014-11-16 21:34:05 -0800

    Add margins between the fields

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Sites/SiteFragment.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Sites/SiteFragment.java
@@ -58,7 +58,6 @@ public class SiteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.e("on create view", "called");
         View view = inflater.inflate(R.layout.fragment_site, container, false);
         configureViewWithSite(view, mSite);
         return view;
