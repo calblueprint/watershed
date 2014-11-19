24983fb11618aacdaecb12e5078d3cbcf167c222
Mark Miyashita <negativetwelve@gmail.com>
2014-11-19 02:06:20 -0800

    Specify serializers in the controller for field_reports

--- watershed-rails/app/controllers/api/v1/field_reports_controller.rb
+++ watershed-rails/app/controllers/api/v1/field_reports_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::FieldReportsController < Api::V1::BaseController
   load_and_authorize_resource param_method: :field_report_params
 
-def index
-    render json: @field_reports
+  def index
+    render json: @field_reports, each_serializer: FieldReportListSerializer
+  end
+
+  def show
+    render json: @field_report, serializer: FieldReportSerializer
   end
 
   def create
     if @field_report.save
-      render json: @field_report
+      render json: @field_report, serializer: FieldReportSerializer
     else
       render json: { errors: @field_report.errors }, status: 422
     end
@@ -15,7 +19,7 @@ def index
 
   def update
     if @field_report.update(field_report_params)
-      render json: @field_report
+      render json: @field_report, serializer: FieldReportSerializer
     else
       render json: { errors: @field_report.errors }, status: 422
     end
