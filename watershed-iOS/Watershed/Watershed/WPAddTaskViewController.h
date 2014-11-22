206129497c9685f645ddca748c73de0aeaeb9f14
Jordeen Chang <jordeenchang@gmail.com>
2014-11-21 23:06:54 -0800

    adding new files for creating task view

---
+++ watershed-iOS/Watershed/Watershed/WPAddTaskViewController.h
@@ -0,0 +1,14 @@
+//
+//  WPAddTaskViewController.h
+//  Watershed
+//
+//  Created by Jordeen Chang on 11/21/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIExtensions.h"
+
+@interface WPAddTaskViewController : WPViewController
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPAddTaskViewController.m
@@ -0,0 +1,27 @@
+//
+//  WPAddTaskViewController.m
+//  Watershed
+//
+//  Created by Jordeen Chang on 11/21/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPAddTaskViewController.h"
+
+@interface WPAddTaskViewController ()
+
+@end
+
+@implementation WPAddTaskViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
