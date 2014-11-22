07ce00ad3bb75257e4fa18676cfdceb10670a66c
Mark Miyashita <negativetwelve@gmail.com>
2014-11-22 15:07:36 -0800

    Add facebook id to users

--- watershed-rails/db/schema.rb
+++ watershed-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122223012) do
+ActiveRecord::Schema.define(version: 20141122230618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20141122223012) do
     t.integer  "role",                   default: 0
     t.string   "authentication_token"
     t.string   "facebook_auth_token"
+    t.string   "facebook_id"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
