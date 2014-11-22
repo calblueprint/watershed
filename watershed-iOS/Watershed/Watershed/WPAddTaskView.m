b89011850a21659ef24b887fee7afb2acfacb660
Jordeen Chang <jordeenchang@gmail.com>
2014-11-21 23:06:54 -0800

    adding new files for creating task view

---
+++ watershed-iOS/Watershed/Watershed/WPAddTaskView.m
@@ -0,0 +1,14 @@
+//
+//  WPAddTaskView.m
+//  Watershed
+//
+//  Created by Jordeen Chang on 11/21/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPAddTaskView.h"
+
+@implementation WPAddTaskView
+
+
+@end
