c9c9f38b43b5aa8d0a52755fcb67779a29f2632c
Mark Miyashita <negativetwelve@gmail.com>
2014-11-23 21:33:20 -0800

    Add image_tmp to photos

--- watershed-rails/db/schema.rb
+++ watershed-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123012632) do
+ActiveRecord::Schema.define(version: 20141124053251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141123012632) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "original_filename"
+    t.string   "image_tmp"
   end
 
   create_table "sites", force: true do |t|
