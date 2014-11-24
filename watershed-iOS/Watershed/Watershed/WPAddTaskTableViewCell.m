7ec704f17c4ece370c92ba831dd9441c5e2e0842
Jordeen Chang <jordeenchang@gmail.com>
2014-11-23 19:54:45 -0800

    constants for heading and paragraph colors

--- watershed-iOS/Watershed/Watershed/WPAddTaskTableViewCell.m
+++ watershed-iOS/Watershed/Watershed/WPAddTaskTableViewCell.m
@@ -12,6 +12,7 @@
 
 @implementation WPAddTaskTableViewCell
 
+NSInteger labelWidth = 90;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *) reuseIdentifier
     andControl: (UIControl *) control {
@@ -30,7 +31,7 @@
 - (void)createSubviews {
     _label = [({
         UILabel *label = [[UILabel alloc] init];
-        label.textColor = [UIColor grayColor];
+        label.textColor = [UIColor wp_header];
         label.font = [UIFont boldSystemFontOfSize:16];
         label;
     }) wp_addToSuperview:self.contentView];
@@ -39,9 +40,9 @@
 - (void)updateConstraints {
 
     [self.label mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.contentView.mas_top).with.offset(standardMargin);
+        make.centerY.equalTo(self.contentView.mas_centerY);
         make.left.equalTo(self.contentView.mas_leading).with.offset(standardMargin);
-        make.width.equalTo(@80);
+        make.width.equalTo(@(labelWidth));
     }];
     
     [self.control mas_makeConstraints:^(MASConstraintMaker *make) {
