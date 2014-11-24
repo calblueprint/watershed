038dc138a62f6d13f04f7a10f7e4333bc26a9c59
Mark Miyashita <negativetwelve@gmail.com>
2014-11-23 20:47:19 -0800

    Change health to healthRating. Make the id not part of the post request

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/FieldReports/FieldReport.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/FieldReports/FieldReport.java
@@ -5,7 +5,9 @@ import android.graphics.Bitmap;
 import com.blueprint.watershed.APIObject;
 import com.blueprint.watershed.MiniSites.MiniSite;
 import com.blueprint.watershed.Users.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.lang.reflect.Field;
 
@@ -18,7 +20,7 @@ public class FieldReport implements APIObject {
     // Attributes
     private Integer mId;
     private String mDescription;
-    private Integer mHealth;
+    private Integer mHealthRating;
     private Boolean mUrgent;
     //private Bitmap mPhoto;
 
@@ -29,10 +31,10 @@ public class FieldReport implements APIObject {
     public FieldReport() {
     }
 
-    public FieldReport(String description, Integer health, Boolean urgent, Bitmap photo,
+    public FieldReport(String description, Integer healthRating, Boolean urgent, Bitmap photo,
                        User user, MiniSite miniSite) {
         mDescription = description;
-        mHealth = health;
+        mHealthRating = healthRating;
         mUrgent = urgent;
         //mPhoto = photo;
         mUser = user;
@@ -44,11 +46,17 @@ public class FieldReport implements APIObject {
      */
 
     // Getters
+    @JsonIgnore
     public User getUser() { return mUser; }
+
+    @JsonIgnore
     public MiniSite getMiniSite() { return mMiniSite; }
 
     // Setters
+    @JsonProperty
     public void setUser(User user) { mUser = user; }
+
+    @JsonProperty
     public void setMiniSite(MiniSite miniSite) { mMiniSite = miniSite; }
 
     /*
@@ -56,12 +64,14 @@ public class FieldReport implements APIObject {
      */
 
     // Getters
+    @JsonIgnore
     public Integer getId() { return mId; }
+
     public String getDescription() {
         return mDescription;
     }
-    public Integer getHealth() {
-        return mHealth;
+    public Integer getHealthRating() {
+        return mHealthRating;
     }
     public Boolean getUrgent() {
         return mUrgent;
@@ -71,12 +81,14 @@ public class FieldReport implements APIObject {
 //    }
 
     // Setters
+    @JsonProperty
     public void setId(Integer id) { mId = id; }
+
     public void setDescription(String description) {
         mDescription = description;
     }
-    public void setHealth(Integer health) {
-        mHealth = health;
+    public void setHealthRating(Integer healthRating) {
+        mHealthRating = healthRating;
     }
     public void setUrgent(Boolean urgent) {
         mUrgent = urgent;
