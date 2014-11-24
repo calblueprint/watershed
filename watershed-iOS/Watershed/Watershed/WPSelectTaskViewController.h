983d6fbe81f7256666bb367b358edda8fd7c4f5b
Jordeen Chang <jordeenchang@gmail.com>
2014-11-23 20:14:53 -0800

    adding view for selecting a task

---
+++ watershed-iOS/Watershed/Watershed/WPSelectTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPSelectTaskViewController.h
+//  Watershed
+//
+//  Created by Jordeen Chang on 11/23/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPViewController.h"
+
+@interface WPSelectTaskViewController : WPViewController
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPSelectTaskViewController.m
@@ -0,0 +1,26 @@
+//
+//  WPSelectTaskViewController.m
+//  Watershed
+//
+//  Created by Jordeen Chang on 11/23/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPSelectTaskViewController.h"
+
+@interface WPSelectTaskViewController ()
+
+@end
+
+@implementation WPSelectTaskViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
