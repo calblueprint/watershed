3dfd184bad9cccb814769c2faabeaa03b8224f6a
Max Wolffe <max.alan.wolffe@gmail.com>
2014-11-23 18:27:57 -0800

    drawer closes when item selected

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Activities/MainActivity.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Activities/MainActivity.java
@@ -361,6 +361,7 @@ public class MainActivity extends ActionBarActivity
                 startActivity(intent);
                 break;
         }
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 
     @Override
