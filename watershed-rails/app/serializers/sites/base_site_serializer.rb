badcf44a7ed8197ff3f049a33ab350970411f7a9
Mark Miyashita <negativetwelve@gmail.com>
2014-11-30 20:35:01 -0700

    Specify the serializer for the photos

--- watershed-rails/app/serializers/sites/base_site_serializer.rb
+++ watershed-rails/app/serializers/sites/base_site_serializer.rb
@@ -1,5 +1,7 @@
 class BaseSiteSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :street, :city,
              :state, :zip_code, :latitude, :longitude,
-             :mini_sites_count, :photos
+             :mini_sites_count
+
+  has_many :photos, serializer: PhotoSerializer
 end
