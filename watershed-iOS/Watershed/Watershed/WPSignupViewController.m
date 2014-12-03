f250ddc178e4867ead4940f18fd3d0e7a12f8ed6
Melissa Huang <melissahuang10@gmail.com>
2014-12-02 20:40:00 -0800

    email signup

--- watershed-iOS/Watershed/Watershed/WPSignupViewController.m
+++ watershed-iOS/Watershed/Watershed/WPSignupViewController.m
@@ -35,23 +35,37 @@
 
 - (void)createSubviews {
     _nameField = [[UITextField alloc] init];
-    _nameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Full Name"
+    _nameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@" Full Name"
                                                                         attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
     _nameField.font = [UIFont fontWithName:@"Helvetica" size:12];
     _nameField.textColor = [UIColor whiteColor];
     _nameField.backgroundColor = [UIColor wp_transBlack];
     [self addSubview:_nameField];
     
+    _emailField = [[UITextField alloc] init];
+    _emailField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@" Email address"
+                                                                       attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    _emailField.font = [UIFont fontWithName:@"Helvetica" size:12];
+    _emailField.textColor = [UIColor whiteColor];
+    _emailField.backgroundColor = [UIColor wp_transBlack];
+    [self addSubview:_emailField];
+    
 }
 
 - (void)updateConstraints {
     [self.nameField mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(@(topMargin+standardMargin));
+        make.top.equalTo(@(topMargin+2*standardMargin));
         make.leading.equalTo(@30);
         make.trailing.equalTo(@-30);
         make.height.equalTo(@40);
     }];
     
+    [self.emailField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.nameField.mas_bottom).with.offset(standardMargin);
+        make.leading.equalTo(self.nameField.mas_leading);
+        make.trailing.equalTo(self.nameField.mas_trailing);
+        make.height.equalTo(self.nameField.mas_height);
+    }];
     
     [super updateConstraints];
 }
