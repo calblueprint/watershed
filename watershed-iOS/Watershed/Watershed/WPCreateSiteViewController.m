73d6ca45d2aed50fb4c880927d2768b46b7f3960
Andrew Millman <andrewmillman35@gmail.com>
2014-12-02 22:54:59 -0800

    CLAIM THA NAME

--- watershed-iOS/Watershed/Watershed/WPCreateSiteViewController.m
+++ watershed-iOS/Watershed/Watershed/WPCreateSiteViewController.m
@@ -2,7 +2,7 @@
 //  WPCreateSiteViewController.m
 //  Watershed
 //
-//  Created by Andrew on 12/2/14.
+//  Created by Andrew Millman on 12/2/14.
 //  Copyright (c) 2014 Blueprint. All rights reserved.
 //
 
