23d3854026368c957153a82da9042eff04f569e6
Melissa Huang <melissahuang10@gmail.com>
2014-12-02 20:43:59 -0800

    text field padding

--- watershed-iOS/Watershed/Watershed/WPSignupViewController.m
+++ watershed-iOS/Watershed/Watershed/WPSignupViewController.m
@@ -34,21 +34,39 @@
 }
 
 - (void)createSubviews {
+    
+    UIView *spacerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
+
+    
     _nameField = [[UITextField alloc] init];
-    _nameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@" Full Name"
-                                                                        attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    _nameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Full Name"
+                                                                        attributes:@{NSForegroundColorAttributeName:[UIColor lightGrayColor]}];
     _nameField.font = [UIFont fontWithName:@"Helvetica" size:12];
     _nameField.textColor = [UIColor whiteColor];
     _nameField.backgroundColor = [UIColor wp_transBlack];
+    [_nameField setLeftViewMode:UITextFieldViewModeAlways];
+    [_nameField setLeftView:spacerView];
     [self addSubview:_nameField];
     
     _emailField = [[UITextField alloc] init];
-    _emailField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@" Email address"
-                                                                       attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    _emailField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Email address"
+                                                                       attributes:@{NSForegroundColorAttributeName:[UIColor lightGrayColor]}];
     _emailField.font = [UIFont fontWithName:@"Helvetica" size:12];
     _emailField.textColor = [UIColor whiteColor];
     _emailField.backgroundColor = [UIColor wp_transBlack];
+    [_emailField setLeftViewMode:UITextFieldViewModeAlways];
+    [_emailField setLeftView:spacerView];
     [self addSubview:_emailField];
+    
+    _passwordField = [[UITextField alloc] init];
+    _passwordField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Password"
+                                                                        attributes:@{NSForegroundColorAttributeName:[UIColor lightGrayColor]}];
+    _passwordField.font = [UIFont fontWithName:@"Helvetica" size:12];
+    _passwordField.textColor = [UIColor whiteColor];
+    _passwordField.backgroundColor = [UIColor wp_transBlack];
+    [_passwordField setLeftViewMode:UITextFieldViewModeAlways];
+    [_passwordField setLeftView:spacerView];
+    [self addSubview:_passwordField];
     
 }
 
@@ -62,6 +80,13 @@
     
     [self.emailField mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.nameField.mas_bottom).with.offset(standardMargin);
+        make.leading.equalTo(self.nameField.mas_leading);
+        make.trailing.equalTo(self.nameField.mas_trailing);
+        make.height.equalTo(self.nameField.mas_height);
+    }];
+    
+    [self.passwordField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.emailField.mas_bottom).with.offset(standardMargin);
         make.leading.equalTo(self.nameField.mas_leading);
         make.trailing.equalTo(self.nameField.mas_trailing);
         make.height.equalTo(self.nameField.mas_height);
