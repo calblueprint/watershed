c8530d26969d26898b449515d3b53213ecccd270
Mark Miyashita <negativetwelve@gmail.com>
2014-12-03 20:54:59 -0800

    Add field reports and tasks for a specific user endpoint

---
+++ watershed-rails/app/controllers/api/v1/users/field_reports_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Users::FieldReportsController < Api::V1::Users::BaseController
+  load_and_authorize_resource
+
+  def index
+  end
+
+end
---
+++ watershed-rails/app/controllers/api/v1/users/tasks_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Users::TasksController < Api::V1::Users::BaseController
+  load_and_authorize_resource
+
+  def index
+  end
+
+end
