56dfcbb24c560aa6af5538289932e64615cdc416
Mark Miyashita <negativetwelve@gmail.com>
2014-12-03 19:29:37 -0800

    Change facebook auth token to text

--- watershed-rails/db/schema.rb
+++ watershed-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124053251) do
+ActiveRecord::Schema.define(version: 20141204032810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20141124053251) do
     t.string   "name"
     t.integer  "role",                   default: 0
     t.string   "authentication_token"
-    t.string   "facebook_auth_token"
+    t.text     "facebook_auth_token"
     t.string   "facebook_id"
   end
 
