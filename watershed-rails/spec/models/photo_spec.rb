17e0587405727fb017e8eb26ea48b08465b14575
Mark Miyashita <negativetwelve@gmail.com>
2014-12-04 21:45:14 -0800

    Add default scope to hide hidden photos and annotate photo model

--- watershed-rails/spec/models/photo_spec.rb
+++ watershed-rails/spec/models/photo_spec.rb
@@ -10,6 +10,7 @@
 #  updated_at        :datetime
 #  original_filename :string(255)
 #  image_tmp         :string(255)
+#  hidden            :boolean          default(FALSE)
 #
 
 require 'spec_helper'
