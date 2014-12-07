6be877e35fba874a018ddd51d2c37673e5db8a5f
Mark Miyashita <negativetwelve@gmail.com>
2014-12-07 14:42:46 -0800

    Add created_at to json responses for tasks, users, photos, field_reports

--- watershed-rails/app/serializers/tasks/base_task_serializer.rb
+++ watershed-rails/app/serializers/tasks/base_task_serializer.rb
@@ -1,6 +1,6 @@
 class BaseTaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :site_id,
-             :complete, :due_date, :urgent
+             :complete, :due_date, :urgent, :created_at
 
   has_one :assignee, serializer: UserListSerializer
   has_one :assigner, serializer: UserListSerializer
