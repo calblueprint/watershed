dbed22d1beb95e914b930330007efefff1cf8bff
Melissa Huang <melissahuang10@gmail.com>
2014-12-07 14:51:11 -0800

    Task model

---
+++ watershed-iOS/Watershed/Watershed/WPTask.h
@@ -0,0 +1,13 @@
+//
+//  WPTask.h
+//  Watershed
+//
+//  Created by Melissa Huang on 12/7/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface WPTask : MTLModel
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPTask.m
@@ -0,0 +1,13 @@
+//
+//  WPTask.m
+//  Watershed
+//
+//  Created by Melissa Huang on 12/7/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPTask.h"
+
+@implementation WPTask
+
+@end
