fe742de337f1fc7867f590edd16d16b68dfbcb00
Mark Miyashita <negativetwelve@gmail.com>
2014-12-08 11:39:18 -0800

    Tasks have a mini site id

--- watershed-rails/app/controllers/api/v1/tasks_controller.rb
+++ watershed-rails/app/controllers/api/v1/tasks_controller.rb
@@ -29,7 +29,7 @@ class Api::V1::TasksController < Api::V1::BaseController
   private
 
   def site_params
-    params.require(:task).permit(:title, :description, :site_id,
+    params.require(:task).permit(:title, :description, :mini_site_id,
                                  :assigner_id, :assignee_id, :complete,
                                  :due_date)
   end
