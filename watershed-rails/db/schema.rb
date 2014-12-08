a45db51a13ab0a321a49520b10bc73ddbd8d10b0
Mark Miyashita <negativetwelve@gmail.com>
2014-12-08 01:38:39 -0800

    Add planted_at to mini_sites

--- watershed-rails/db/schema.rb
+++ watershed-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208092335) do
+ActiveRecord::Schema.define(version: 20141208093807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141208092335) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "site_id"
+    t.date     "planted_at"
   end
 
   create_table "photos", force: true do |t|
