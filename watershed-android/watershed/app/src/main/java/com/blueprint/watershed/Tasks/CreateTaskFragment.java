5d96e728214c7c6d7fd9b32d4c5f4b283fe99172
Max Wolffe <max.alan.wolffe@gmail.com>
2014-12-07 21:59:28 -0800

    add create task view

---
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Tasks/CreateTaskFragment.java
@@ -0,0 +1,60 @@
+package com.blueprint.watershed.Tasks;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.blueprint.watershed.R;
+
+
+public class CreateTaskFragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public static CreateTaskFragment newInstance() {
+        CreateTaskFragment fragment = new CreateTaskFragment();
+        return fragment;
+    }
+    public CreateTaskFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_task, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
