ab212c3eb665eb1924f6b4124f4d6d93f7784c5f
Mark Miyashita <negativetwelve@gmail.com>
2014-12-08 16:40:03 -0800

    Return photos for a mini site show view

--- watershed-rails/app/serializers/mini_sites/mini_site_serializer.rb
+++ watershed-rails/app/serializers/mini_sites/mini_site_serializer.rb
@@ -1,5 +1,6 @@
 class MiniSiteSerializer < BaseMiniSiteSerializer
   has_many :field_reports, each_serializer: FieldReportListSerializer
+  has_many :photos, each_serializer: PhotoSerializer
 
   # has_many :users
 end
