a540f7f4ec029e6b86d6ffbdc365a4f56ca1e7a4
Andrew Millman <andrewmillman35@gmail.com>
2014-12-11 18:49:58 -0800

    Add createMiniSiteView

---
+++ watershed-iOS/Watershed/Watershed/WPCreateMiniSiteView.h
@@ -0,0 +1,13 @@
+//
+//  WPCreateMiniSiteView.h
+//  Watershed
+//
+//  Created by Andrew Millman on 12/9/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPView.h"
+
+@interface WPCreateMiniSiteView : WPView
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPCreateMiniSiteView.m
@@ -0,0 +1,21 @@
+//
+//  WPCreateMiniSiteView.m
+//  Watershed
+//
+//  Created by Andrew Millman on 12/9/14.
+//  Copyright (c) 2014 Blueprint. All rights reserved.
+//
+
+#import "WPCreateMiniSiteView.h"
+
+@implementation WPCreateMiniSiteView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
