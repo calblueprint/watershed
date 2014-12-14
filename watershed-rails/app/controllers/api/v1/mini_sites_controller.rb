b4c629afd4343e74240f594ecec2fab3caeb00a8
Mark Miyashita <negativetwelve@gmail.com>
2014-12-13 18:40:21 -0800

    Allow photo attributes for mini sites and convert data to an image

--- watershed-rails/app/controllers/api/v1/mini_sites_controller.rb
+++ watershed-rails/app/controllers/api/v1/mini_sites_controller.rb
@@ -28,10 +28,20 @@ class Api::V1::MiniSitesController < Api::V1::BaseController
   private
 
   def mini_site_params
-    params.require(:mini_site).permit(:id, :name, :description,
+    params.require(:mini_site).permit(:name, :description,
                                       :street, :city, :state,
                                       :zip_code, :latitude, :longitude,
-                                      :site_id)
+                                      :site_id, {
+                                        photo_attributes: [
+                                          :id,
+                                          :image,
+                                        ],
+                                      })
+  end
+
+  def convert_base64_to_image
+    # TODO(mark): Consider making this a helper function for all models
+    params[:mini_site][:photo_attributes][:image] = Photo.convert_base64(params[:mini_site][:photo_attributes][:data])
   end
 
 end
