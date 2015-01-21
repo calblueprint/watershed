8db9102d418c9c08729489e6240084e88b447827
Andrew Millman <andrewmillman35@gmail.com>
2015-01-20 16:26:37 -0800

    Adjust sizing of the text in AboutViewController

--- watershed-iOS/Watershed/Watershed/WPAboutViewController.m
+++ watershed-iOS/Watershed/Watershed/WPAboutViewController.m
@@ -12,11 +12,10 @@
 @implementation WPAboutViewController
 
 - (void)viewDidLoad {
-    self.navigationItem.title = @"About";
-    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationItem.title = @"Watershed";
 }
 
--(void)loadView {
+- (void)loadView {
     self.view = [[WPAboutView alloc] init];
 }
 
@@ -38,16 +37,17 @@
 - (void)createSubviews {
     
     _titleLabel = [[UILabel alloc] init];
-    _titleLabel.text = @"THE WATERSHED PROJECT";
-    _titleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:24];
+    _titleLabel.text = @"ABOUT THE WATERSHED PROJECT";
+    _titleLabel.font = [UIFont boldSystemFontOfSize:24.0];
+    _titleLabel.textAlignment = NSTextAlignmentCenter;
     _titleLabel.textColor = [UIColor whiteColor];
     _titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
-    _titleLabel.numberOfLines = 3;
+    _titleLabel.numberOfLines = 0;
     [self addSubview:_titleLabel];
     
     _aboutLabel = [[UILabel alloc] init];
     _aboutLabel.text = @"The Watershed Project's mission is to inspire Bay Area communities to understand, appreciate and protect our local watersheds. Located at the University of California's Richmond Field Station, since 1997 we have pursued this vision through various initiatives. The Healthy Watersheds Initiative focuses on litter and other source of pollutants. The Living Shoreline Initiative aims to help people appreciate the rich potential for healthy underwater habitats in the Bay and along it's shoreline, with an emphasis on native Oyster restoration. The Greening Urban Watersheds Initiative seeks to redefine the way we inhabit our urban centers, envisioning a green cityscape that maintains or mimics the natural flows and rhythms of local ecosystems. ";
-    _aboutLabel.font = [UIFont systemFontOfSize:15];
+    _aboutLabel.font = [UIFont systemFontOfSize:18];
     _aboutLabel.lineBreakMode = NSLineBreakByWordWrapping;
     _aboutLabel.numberOfLines = 0;
     _aboutLabel.textColor = [UIColor whiteColor];
@@ -60,7 +60,7 @@
     [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(@(topMargin + standardMargin));
         make.leading.equalTo(@(standardMargin));
-        make.width.equalTo(@175);
+        make.trailing.equalTo(@(-standardMargin));
     }];
     
     [self.aboutLabel mas_makeConstraints:^(MASConstraintMaker *make) {
