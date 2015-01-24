e83a5bdd24d2fdd1e8c03cb0baa3b5d1f18d3ca2
Andrew Millman <andrewmillman35@gmail.com>
2015-01-24 12:02:49 -0800

    Split WPTermsView and WPTermsViewController to their own files

---
+++ watershed-iOS/Watershed/Watershed/WPTermsView.h
@@ -0,0 +1,18 @@
+//
+//  WPTermsView.h
+//  Watershed
+//
+//  Created by Andrew Millman on 1/24/15.
+//  Copyright (c) 2015 Blueprint. All rights reserved.
+//
+
+#import "WPView.h"
+
+@interface WPTermsView : WPView
+
+@property (nonatomic) UILabel *termsLabel;
+@property (nonatomic) UILabel *termsInfoLabel;
+@property (nonatomic) UILabel *privacyLabel;
+@property (nonatomic) UILabel *privacyInfoLabel;
+
+@end
