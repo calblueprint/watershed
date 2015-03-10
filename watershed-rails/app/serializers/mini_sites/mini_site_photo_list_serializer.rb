2c484452ab12cc3958cdf77d89af07b4373735af
Charles Xue <charlesx2013@gmail.com>
2015-03-10 15:25:09 -0700

    added more serilaizers,and changed controlers

--- watershed-rails/app/serializers/mini_sites/mini_site_photo_list_serializer.rb
+++ watershed-rails/app/serializers/mini_sites/mini_site_photo_list_serializer.rb
@@ -1,4 +1,4 @@
-class MiniSiteListSerializer < BaseMiniSiteSerializer
+class MiniSitePhotoListSerializer < BaseMiniSiteSerializer
   attributes :health_rating
 
   has_many :photos, each_serializer: PhotoSerializer
