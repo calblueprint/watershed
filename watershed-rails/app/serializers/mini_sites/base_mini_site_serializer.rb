fb43eeb73b70a420fc31f2529ac1f7c49b2aa729
Charles Xue <charlesx2013@gmail.com>
2015-03-20 05:18:33 -0700

    dangit mark

--- watershed-rails/app/serializers/mini_sites/base_mini_site_serializer.rb
+++ watershed-rails/app/serializers/mini_sites/base_mini_site_serializer.rb
@@ -1,5 +1,5 @@
 class BaseMiniSiteSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :street, :city,
              :state, :zip_code, :latitude, :longitude,
-             :planted_at, :tasks_count, :field_reports_count
+             :planted_at, :tasks_count, :field_reports_count, :site_id
 end
