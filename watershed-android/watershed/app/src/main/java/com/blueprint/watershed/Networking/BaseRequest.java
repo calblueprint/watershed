6bcdf7154b5576763af7b1bf2279148b2971fd85
Charles Xue <charlesx2013@gmail.com>
2015-03-21 19:24:41 -0700

    staring to add fab

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Networking/BaseRequest.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Networking/BaseRequest.java
@@ -29,7 +29,7 @@ public abstract class BaseRequest extends JsonObjectRequest {
     private Response.Listener listener;
     private Response.Listener errorListener;
 
-    private static final String baseURL = "http://192.168.0.103:3000/api/v1/";
+    private static final String baseURL = "http://192.168.0.102:3000/api/v1/";
 //    private static final String baseURL = "https://intense-reaches-1457.herokuapp.com/api/v1/";
 
     public BaseRequest(int method, String url, JSONObject jsonRequest,
@@ -55,7 +55,7 @@ public abstract class BaseRequest extends JsonObjectRequest {
                     apiError = mapper.readValue(errorJson, new TypeReference<APIError>() {
                     });
                 } catch (Exception e) {
-                    Log.e("Json exception in Base Request", e.toString());
+                    Log.e("Json exception base", e.toString());
                 }
                 errorListener.onResponse(apiError);
 
