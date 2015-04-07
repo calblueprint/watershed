f3cee2b7a7438caea570623e558716c44f1f73d4
Charles Xue <charlesx2013@gmail.com>
2015-04-06 21:08:07 -0700

    added mufasa3d.gif

--- watershed-rails/config/routes.rb
+++ watershed-rails/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root to: "static_pages#index"
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       match "/", to: "base#ping", via: :get
