624d1dfe12d4f523e49df1394a4c0160039c98c2
Charles Xue <charlesx2013@gmail.com>
2015-04-12 23:02:38 -0700

    basic notifcation setup

---
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Notifications/GcmBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.blueprint.watershed.Notifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by charlesx on 4/12/15.
+ */
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Notifications/GcmIntentService.java
@@ -0,0 +1,65 @@
+package com.blueprint.watershed.Notifications;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+import java.io.FileDescriptor;
+
+/**
+ * Created by charlesx on 4/12/15.
+ */
+public class GcmIntentService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new IBinder() {
+            @Override
+            public String getInterfaceDescriptor() throws RemoteException {
+                return null;
+            }
+
+            @Override
+            public boolean pingBinder() {
+                return false;
+            }
+
+            @Override
+            public boolean isBinderAlive() {
+                return false;
+            }
+
+            @Override
+            public IInterface queryLocalInterface(String descriptor) {
+                return null;
+            }
+
+            @Override
+            public void dump(FileDescriptor fd, String[] args) throws RemoteException {
+
+            }
+
+            @Override
+            public void dumpAsync(FileDescriptor fd, String[] args) throws RemoteException {
+
+            }
+
+            @Override
+            public boolean transact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+                return false;
+            }
+
+            @Override
+            public void linkToDeath(DeathRecipient recipient, int flags) throws RemoteException {
+
+            }
+
+            @Override
+            public boolean unlinkToDeath(DeathRecipient recipient, int flags) {
+                return false;
+            }
+        }
+    }
+}
