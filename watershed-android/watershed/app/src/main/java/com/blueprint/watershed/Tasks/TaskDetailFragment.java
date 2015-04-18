cefb40e831b6de4cecdee5a27c86c147845fa6d7
Charles Xue <charlesx2013@gmail.com>
2015-04-17 23:37:21 -0700

    got rid of useless lines

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Tasks/TaskDetailFragment.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Tasks/TaskDetailFragment.java
@@ -126,7 +126,6 @@ public class TaskDetailFragment extends TaskAbstractFragment
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.clear();
         inflater.inflate(R.menu.delete_menu, menu);
-//        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
