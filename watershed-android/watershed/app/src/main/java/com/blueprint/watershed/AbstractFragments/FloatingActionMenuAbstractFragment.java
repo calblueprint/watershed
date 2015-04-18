08e7fb7cc61a78c8e5a46977cb489f27c8c4f467
Charles Xue <charlesx2013@gmail.com>
2015-04-18 02:01:10 -0700

    more documentation for fabmenuabstractfragment

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/AbstractFragments/FloatingActionMenuAbstractFragment.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/AbstractFragments/FloatingActionMenuAbstractFragment.java
@@ -12,10 +12,17 @@ public abstract class FloatingActionMenuAbstractFragment extends Fragment {
 
     public FloatingActionsMenu mMenu;
 
+    /**
+     * Checks if mMenu is open
+     * @return Boolean that tells you whether or not the menu was open
+     */
     public boolean isMenuOpen() {
         return mMenu.isExpanded();
     }
 
+    /**
+     * Closes mMenu
+     */
     public void closeMenu() {
         mMenu.collapse();
     }
