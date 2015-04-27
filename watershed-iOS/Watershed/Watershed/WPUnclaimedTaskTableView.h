0a85f8c572620e7d67b82d7562cd57dbd6fe81f0
Jordeen Chang <jordeenchang@gmail.com>
2015-04-26 20:23:28 -0700

    adding unclaimed tasks table view

---
+++ watershed-iOS/Watershed/Watershed/WPUnclaimedTaskTableView.h
@@ -0,0 +1,13 @@
+//
+//  WPUnclaimedTaskTableView.h
+//  Watershed
+//
+//  Created by Jordeen Chang on 4/26/15.
+//  Copyright (c) 2015 Blueprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPUnclaimedTaskTableView : UITableView
+
+@end
---
+++ watershed-iOS/Watershed/Watershed/WPUnclaimedTaskTableView.m
@@ -0,0 +1,13 @@
+//
+//  WPUnclaimedTaskTableView.m
+//  Watershed
+//
+//  Created by Jordeen Chang on 4/26/15.
+//  Copyright (c) 2015 Blueprint. All rights reserved.
+//
+
+#import "WPUnclaimedTaskTableView.h"
+
+@implementation WPUnclaimedTaskTableView
+
+@end
