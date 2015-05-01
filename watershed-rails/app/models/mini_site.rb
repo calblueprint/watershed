a1b578a32fd05ed649abdee91c92f0fe1d3e40b5
Charles Xue <charlesx2013@gmail.com>
2015-04-30 20:39:25 -0700

    mini site validations

--- watershed-rails/app/models/mini_site.rb
+++ watershed-rails/app/models/mini_site.rb
@@ -18,20 +18,27 @@
 #
 
 class MiniSite < ActiveRecord::Base
-  belongs_to :site
+  accepts_nested_attributes_for :photos
 
   has_many :field_reports, dependent: :destroy
-
   has_many :user_mini_sites, dependent: :destroy
   has_many :users, through: :user_mini_sites
-
   has_many :tasks, dependent: :destroy
-
   has_many :photos, as: :parent, dependent: :destroy
+
+  belongs_to :site
 
   scope :with_photos, -> { select { |mini_site| mini_site.photos.size > 0 } }
 
-  accepts_nested_attributes_for :photos
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :site_id, presence: true
 
   # TODO(mark): Make these counts into counter caches
   def tasks_count
