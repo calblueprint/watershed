cb4f416b39265a848aa424422d1d6958092cdaf2
Charles Xue <charlesx2013@gmail.com>
2015-05-03 04:09:48 -0700

    styling row

--- watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Users/UserListAdapter.java
+++ watershed-android/watershed/app/src/main/java/com/blueprint/watershed/Users/UserListAdapter.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.blueprint.watershed.R;
-import android.widget.LinearLayout;
 
 import java.util.List;
 
