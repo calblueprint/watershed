63c3a817995240fedf6bd494ef7bdfab50756ecb
Charles Xue <charlesx2013@gmail.com>
2015-05-03 14:30:48 -0700

    reorganization of routes

--- watershed-rails/config/routes.rb
+++ watershed-rails/config/routes.rb
@@ -24,24 +24,49 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :sites, except: [:new, :edit] do
-        member do
-          post :subscribe
-          delete :unsubscribe
+      namespace :managers do
+        resources :sites, except: [:new, :edit] do
+          member do
+            post :subscribe
+            delete :unsubscribe
+          end
+
+          collection do
+            get :search
+          end
         end
 
-        collection do
-          get :search
+        resources :tasks, except: [:new, :edit] do
+          member do
+            post :claim
+          end
         end
+
+        resources :mini_sites,    except: [:new, :edit]
+        resources :field_reports, except: [:new, :edit, :destroy]
       end
 
-      resources :tasks, except: [:new, :edit] do
-        member do
-          post :claim
+      namespace :community_members do
+        resources :sites, except: [:new, :edit] do
+          member do
+            post :subscribe
+            delete :unsubscribe
+          end
+
+          collection do
+            get :search
+          end
         end
+
+        resources :tasks, except: [:new, :edit] do
+          member do
+            post :claim
+          end
+        end
+
+        resources :mini_sites,    except: [:new, :edit]
+        resources :field_reports, except: [:new, :edit, :destroy]
       end
-      resources :mini_sites,    except: [:new, :edit]
-      resources :field_reports, except: [:new, :edit, :destroy]
     end
   end
 end
