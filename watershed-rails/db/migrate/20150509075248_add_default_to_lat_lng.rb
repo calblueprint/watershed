dd37b91d2f1ee077baefc960538fff62e95bbc01
Charles Xue <charlesx2013@gmail.com>
2015-05-09 01:05:31 -0700

    changing to float

--- watershed-rails/db/migrate/20150509075248_add_default_to_lat_lng.rb
+++ watershed-rails/db/migrate/20150509075248_add_default_to_lat_lng.rb
@@ -1,4 +1,6 @@
 class AddDefaultToLatLng < ActiveRecord::Migration
   def change
+    change_column :sites, :latitude, precision: 10, scale: 6, default: 0
+    change_column :sites, :longitude, precision: 10, scale: 6, default: 0
   end
 end
